f0400c41468aad860a53e77321f7ec0cd9ed5537
Jessi Ijelman Lipovetzky <46876160@est.ort.edu.ar>
2022-05-09 11:58:12 -0300

    finished

---
+++ Assets/Instantiator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiator : MonoBehaviour
+{
+    public GameObject objectToClone;
+    public int CantDeClones;
+
+    public void CloneObject()
+    {
+        while (CantDeClones > 0)
+        {
+            Instantiate(objectToClone);
+            CantDeClones--;
+        }
+    }
+}
